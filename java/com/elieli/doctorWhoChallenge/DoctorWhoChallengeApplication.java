c7e9555df9c37135f13fa4b256f71ef10ddcd4b0
BenPrevel <403rutabaga@gmail.com>
2021-12-20 15:49:59 +0100

    Quest Spring 1 done

---
+++ java/com/elieli/doctorWhoChallenge/DoctorWhoChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.elieli.doctorWhoChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorWhoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorWhoChallengeApplication.class, args);
+	}
+
+}
